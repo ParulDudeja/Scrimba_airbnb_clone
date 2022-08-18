d218ea629ddd733d41b08b3143dcd2b2b0b5f682
Parul Dudeja <paruldudeja93@gmail.com>
2022-08-18 17:03:33 +0400

    minor changes in card

--- p2/src/App.js
+++ p2/src/App.js
@@ -3,14 +3,27 @@ import "./App.css";
 import Card from "./Components/Card";
 import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
+import dataCard from "./dataCard";
 
 function App() {
+  const cards = dataCard.map((item) => {
+    return (
+      <Card
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    );
+  });
   return (
     <div>
       {/* <h1>Hello From App.js</h1> */}
       <Navbar />
       <Hero />
-      <Card />
+      {cards}
     </div>
   );
 }
--- p2/src/Components/Card.js
+++ p2/src/Components/Card.js
@@ -1,25 +1,27 @@
 import React from "react";
 import "../css/style.css";
-import Image1 from "../img/img1.png";
+// import Image1 from "../img/img1.png";
 import starImage from "../img/star1.png";
+import dataCard from "../dataCard";
 
-export default function Card() {
+export default function Card(props) {
+  const data = dataCard.map((val) => console.log(data));
   return (
     <div className="card">
       <img
-        src={Image1}
+        src={props.img}
         alt="Life lessons with Katie Zaferes "
         className="card--image"
       />
       <div className="card--stats">
         <img src={starImage} alt="star" className="card--star" />
-        <span>5.0</span>
-        <span className="lightGrey">(6) • </span>
-        <span className="lightGrey"> USA</span>
+        <span>{props.rating}</span>
+        <span className="gray">({props.reviewCount}) • </span>
+        <span className="gray">{props.location}</span>
       </div>
-      <p>Life lessons with Katie Zaferes</p>
+      <p>{props.title}</p>
       <p>
-        <span className="boldBlack">From $136 </span>/ person
+        <span className="bold">From ${props.price}</span> / person
       </p>
     </div>
   );
