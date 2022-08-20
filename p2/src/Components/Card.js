5e18937b25d333f2eccc5a97c6c76c1797c457a1
Parul Dudeja <paruldudeja93@gmail.com>
2022-08-20 13:21:19 +0400

    pass object as props in Card component

--- p2/src/Components/Card.js
+++ p2/src/Components/Card.js
@@ -6,9 +6,9 @@ import starImage from "../imga/star1.png";
 
 export default function Card(props) {
   let badgeText;
-  if (props.openSpots === 0) {
+  if (props.item.openSpots === 0) {
     badgeText = "SOLD OUT";
-  } else if (props.location === "Online") {
+  } else if (props.item.location === "Online") {
     badgeText = "ONLINE";
   }
   //console.log(props.openSpots);
@@ -17,16 +17,20 @@ export default function Card(props) {
       {/* condional statement in jxs using props  if first is true only it work otherwise it will not*/}
       {/* {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>} */}
       {badgeText && <div className="card--badge">{badgeText}</div>}
-      <img src={`../img/${props.img}`} className="card--image" alt="card" />
+      <img
+        src={`../img/${props.item.coverImg}`}
+        className="card--image"
+        alt="card"
+      />
       <div className="card--stats">
         <img src={starImage} className="card--star" alt="star" />
-        <span>{props.rating}</span>
-        <span className="lightGrey">({props.reviewCount}) • </span>
-        <span className="lightGrey">{props.location}</span>
+        <span>{props.item.stats.rating}</span>
+        <span className="lightGrey">({props.item.stats.reviewCount}) • </span>
+        <span className="lightGrey">{props.item.location}</span>
       </div>
-      <p>{props.title}</p>
+      <p>{props.item.title}</p>
       <p>
-        <span className="boldBlack">From ${props.price}</span> / person
+        <span className="boldBlack">From ${props.item.price}</span> / person
       </p>
     </div>
   );
