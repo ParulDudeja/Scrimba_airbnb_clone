c60b8649ae9ebb99396df9ea916b6a5de48d98fa
Parul Dudeja <paruldudeja93@gmail.com>
2022-08-20 13:08:38 +0400

    badgetext conditional

--- p2/src/Components/Card.js
+++ p2/src/Components/Card.js
@@ -5,8 +5,18 @@ import starImage from "../imga/star1.png";
 // import dataCard from "../dataCard";
 
 export default function Card(props) {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
+  //console.log(props.openSpots);
   return (
     <div className="card">
+      {/* condional statement in jxs using props  if first is true only it work otherwise it will not*/}
+      {/* {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>} */}
+      {badgeText && <div className="card--badge">{badgeText}</div>}
       <img src={`../img/${props.img}`} className="card--image" alt="card" />
       <div className="card--stats">
         <img src={starImage} className="card--star" alt="star" />
